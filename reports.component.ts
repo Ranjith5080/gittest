0194561a635b2794801958290430e7212819b8d5
Ranjith5080 <34800712+Ranjith5080@users.noreply.github.com>
2018-11-19 11:09:44 -0500

    Add files via upload
    report & types

---
+++ reports.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { IReport } from './report';
+import { ReportService } from './reports.service';
+
+
+@Component({
+  selector: 'app-reports',
+  templateUrl: './reports.component.html',
+  styleUrls: ['./reports.component.scss'],
+  providers:[ReportService]
+})
+
+export class ReportsComponent implements OnInit {
+  reports: IReport[];
+  
+  constructor(private _reportService:ReportService){ 
+  }
+  
+  /*trackById(index:number, reports:IReport[]): string{
+    return reports[[]].id;
+  }*/
+ 
+  
+  ngOnInit() {
+    console.log("accessing service call...")
+    this._reportService.getReports()
+      .subscribe(res => this.reports = <IReport[]> res);
+    console.log(this.reports)
+  }
+}
